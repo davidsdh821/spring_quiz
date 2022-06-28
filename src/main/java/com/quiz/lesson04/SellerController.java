0ef12af8b3a5699892dc256457faa6a9a8204a5f
DESKTOP-3BRQ0CN\user <aaaa@gmail.com>
2022-06-28 18:44:21 +0900

    jsp 퀴즈 선행 세팅

---
+++ src/main/java/com/quiz/lesson04/SellerController.java
@@ -0,0 +1,14 @@
+package com.quiz.lesson04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/lesson04")
+@Controller
+public class SellerController {
+
+	@RequestMapping(path =  "/quiz01/1")
+	public 
+	
+	
+}
