52ad39b6876bb77182126a36faa9ffe9d065afc6
DESKTOP-3BRQ0CN\user <aaaa@gmail.com>
2022-06-29 18:46:00 +0900

    jsp 1번 퀴즈 문제 전부 품

--- src/main/java/com/quiz/QuizApplication.java
+++ src/main/java/com/quiz/QuizApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//상위 패키지에 오류제거를 만들어 두고 하위 패키지에서 작업을 할 수 있다.
-//상위 패키지의 클래스부터 작동 하기 때문에 가능하다.
+
 public class QuizApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/quiz/lesson04/SellerController.java
+++ src/main/java/com/quiz/lesson04/SellerController.java
@@ -1,14 +1,68 @@
 package com.quiz.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping("/lesson04")
+import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.model.Seller;
+
+@RequestMapping("/lesson04/quiz01")
 @Controller
 public class SellerController {
-
-	@RequestMapping(path =  "/quiz01/1")
-	public 
+	@Autowired
+	private SellerBO sellerBO;
 	
+	//http://localhost/lesson04/quiz01/1
+	@RequestMapping("/1")
+	public  String addSeller() {
+		
+		return "lesson04/addSeller";
+	
+	}
+	//http://localhost/lesson04/quiz01/add_seller
+	@PostMapping("/add_seller")
+	public String addSeller_info(
+			@RequestParam("nickname") String nickname,
+			@RequestParam(value = "profileImageUrl" , required=false) String profileImageUrl,
+			@RequestParam("temperature") double temperature) {
+		
+		sellerBO.addSeller(nickname, profileImageUrl, temperature);
+		
+		
+		return "lesson04/after_add_seller";
+	}
+	//http://localhost/lesson04/quiz01/2
+	@GetMapping("/2")
+	public String getSeller_1(Model model) {
+		Seller seller = sellerBO.getLastSeller();
+		
+		model.addAttribute("result", seller);
+
+		return "lesson04/getSeller";
+		
+	}
+	///http://localhost/lesson04/quiz01/3
+	@GetMapping("/3")
+	public String getSeller_2(Model model,
+			@RequestParam(value =  "id", required=false) Integer id) {
+		sellerBO.getSeller(id);
+		Seller seller;
+		if(id == null) {
+			seller = sellerBO.getLastSeller();
+		} else {
+			seller = sellerBO.getSeller(id);
+		}
+		
+		model.addAttribute("result", seller);
+		
+		
+		return "lesson04/getSeller";
+	}
+
 	
 }
